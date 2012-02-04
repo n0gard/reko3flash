a8cd0e4ae857e189446b58d7b8db51781fe2f35b
n0gard.wr@gmail.com <n0gard.wr@gmail.com@e8ddec43-6fe1-d5a8-13be-34e34dabbe1b>
2012-02-04 08:46:31 +0000

    new: 增加CPU_emu模拟src

---
+++ trunk/CPU_emu/nes_emu/src/emu/m6502/cpu/register/Accumulator.java
@@ -0,0 +1,15 @@
+package emu.m6502.cpu.register;
+
+/**
+ * 累加器<br>
+ * 
+ * 这是微处理器里最重要的寄存器. 很多种机器语言指令都允许你拷贝内存中某个位置的内容到累加器中，允<br>
+ * 许你拷贝累加器中的内容到内存某个位置中，直接修改累加器或其他寄存器的内容而不影响到任何内存. 累<br>
+ * 加器是唯一有执行数学计算指令的寄存器.
+ * 
+ * @author Tommy.Wang
+ * 
+ */
+public class Accumulator extends Register {
+
+}
---
+++ trunk/CPU_emu/nes_emu/src/emu/m6502/cpu/register/XIndexRegister.java
@@ -0,0 +1,11 @@
+package emu.m6502.cpu.register;
+
+/**
+ * X Ë÷Òý¼Ä´æÆ÷
+ * 
+ * @author Tommy.Wang
+ * 
+ */
+public class XIndexRegister extends Register {
+
+}
